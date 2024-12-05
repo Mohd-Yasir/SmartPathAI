78fbc2abede8f6a92a260d2b0a521b4c8e79c586
Mohd-Yasir <mohdxyasir@gmail.com>
2024-12-06 00:05:36 +0530

    gemini

--- lib/controller/chat_controller.dart
+++ lib/controller/chat_controller.dart
@@ -1,4 +1,34 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+import 'package:flutter_gemini/flutter_gemini.dart';
 
-class ChatController extends GetxController {}
+class ChatController extends GetxController {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  final Gemini gemini = Gemini.instance;
+
+  Future<void> sendMessage(String message) async {
+    User? user = _auth.currentUser;
+    if (user == null) {
+      return;
+    }
+    await _firestore.collection('chats').add({
+      'uid': user.uid,
+      'message': message,
+      'time': FieldValue.serverTimestamp(),
+      'sender': user,
+    });
+
+    Future<String> GetGeminiResponse(String message) async {
+      try {
+        final response = await gemini.prompt(parts: [Part.text(message)]);
+        return response?.output ?? 'No response from Gemini.';
+      } catch (e) {
+        return 'Error: $e';
+      }
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gemini/flutter_gemini.dart';
 import 'package:get/get.dart';
 import 'package:smart_path_ai/firebase_options.dart';
 import 'package:smart_path_ai/view/chatscreen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:smart_path_ai/view/login.dart';
 import 'package:smart_path_ai/view/signup.dart';
-import 'package:smart_path_ai/view/verificationpage.dart';
 
+const String Gemini_API_Key = 'AIzaSyATB6PJuCbH2ob6tE3bz7u8yLQwCvBZg1E';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  Gemini.init(apiKey: Gemini_API_Key);
   runApp(const MyApp());
 }
 
